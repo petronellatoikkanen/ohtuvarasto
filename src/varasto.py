ded1e7cc82ed5c5c4c350712677117e4c077a2f3
Elviira <petronella.toikkanen@gmail.com>
2023-11-11 13:50:35 +0200

    pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,11 @@
 class Varasto:
+
     def __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms
+
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
